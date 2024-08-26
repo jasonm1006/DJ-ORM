263a506f4cf7fc698dac3b51550e59eb844622be
Evgeniy Martyushov <jaysonm10061987@gmail.com>
2024-08-26 23:58:26 +0300

    Initial commit

---
+++ orm_shop/main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from main.models import Car, Client, Sale
+
+admin.site.register(Client)
+admin.site.register(Car)
+admin.site.register(Sale)
+# зарегистрируйте необходимые модели
